35c4841f418ab5f4682f6178a3046133d634453e
DANIEL MALL <danielmall@dan.local.home>
2016-02-29 06:23:42 -0500

    Adding GAiN Conference slides to project hub [deploy:live]

--- hub/index.php
+++ hub/index.php
@@ -1,3 +1,24 @@
+<?php 
+    
+    function timeFromNow($milestone){
+        
+        // http://stackoverflow.com/questions/2040560/finding-the-number-of-days-between-two-dates
+
+        $now = time(); // or your date as well
+        //$your_date = strtotime("2010-01-01");
+        $your_date = strtotime($milestone);
+        $datediff = $now - $your_date;
+        $daysFromNow = floor($datediff/(60*60*24));
+
+        if($daysFromNow < 0){
+            return '&mdash;' . abs($daysFromNow) . " days from now";    
+        }else{
+            return;
+        }
+        
+    }
+
+?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -39,56 +60,65 @@
             <article class="entry entry-future">
             
                 <h1 class="entry-title">Style guide front-end files creation</h1>
-                <p class="entry-date">April 15, 2016</p>
+                <p class="entry-date">April 15, 2016<?php echo timeFromNow('2016-04-15'); ?></p>
                     
             </article><!-- .entry -->
 
             <article class="entry entry-future">
             
                 <h1 class="entry-title">Design exploration</h1>
-                <p class="entry-date">April 7, 2016</p>
+                <p class="entry-date">April 7, 2016<?php echo timeFromNow('2016-04-07'); ?></p>
                     
             </article><!-- .entry -->
 
             <article class="entry entry-future">
             
                 <h1 class="entry-title">Site code refactoring</h1>
-                <p class="entry-date">March 30, 2016</p>
+                <p class="entry-date">March 30, 2016<?php echo timeFromNow('2016-03-30'); ?></p>
                     
             </article><!-- .entry -->
 
             <article class="entry entry-future">
             
                 <h1 class="entry-title">Kickoff</h1>
-                <p class="entry-date">March 16, 2016</p>
+                <p class="entry-date">March 16, 2016<?php echo timeFromNow('2016-03-16'); ?></p>
                     
             </article><!-- .entry -->
 
             <article class="entry entry-future">
             
                 <h1 class="entry-title">Kickoff prep</h1>
-                <p class="entry-date">March 11, 2016</p>
+                <p class="entry-date">March 11, 2016<?php echo timeFromNow('2016-03-11'); ?></p>
                     
             </article><!-- .entry -->
 
             <article class="entry entry-future">
             
                 <h1 class="entry-title">Persona creation</h1>
-                <p class="entry-date">March 2, 2016</p>
+                <p class="entry-date">March 2, 2016<?php echo timeFromNow('2016-03-02'); ?></p>
                     
             </article><!-- .entry -->
 
-            <article class="entry">
+            <article class="entry entry-future">
             
                 <h1 class="entry-title">Collating research findings</h1>
-                <p class="entry-date">Februrary 29, 2016</p>
+                <p class="entry-date">March 2, 2016<?php echo timeFromNow('2016-03-02'); ?></p>
+                    
+            </article><!-- .entry -->
+
+            <article class="entry entry-latest">
+
+                <h1 class="entry-title">GAiN Conference</h1>
+                <p class="entry-date">Februrary 26, 2016<?php echo timeFromNow('2016-02-26'); ?></p>
+
+                <p><a href="http://d.pr/f/18xJD">Dan Mall&rsquo;s Slide deck (PDF&mdash;40.7MB)</a></p>
                     
             </article><!-- .entry -->
 
             <article class="entry">
 
                 <h1 class="entry-title">Stakeholder Interviews &amp; Participant Research</h1>
-                <p class="entry-date">Februrary 26, 2016</p>
+                <p class="entry-date">Februrary 26, 2016<?php echo timeFromNow('2016-02-26'); ?></p>
                     
             </article><!-- .entry -->
 
