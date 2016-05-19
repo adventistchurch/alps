c5e2b47fd6b967eb539ee91602f0097ab143677a
Terrance Pitre <tpitre@southleft.com>
2016-05-19 12:42:56 -0500

    Getting the custom PL styles in good shape.

--- core/styleguide/js/annotations-pattern.js
+++ core/styleguide/js/annotations-pattern.js
@@ -7,7 +7,7 @@
  */
 
 var annotationsPattern = {
-	
+
 	commentsOverlayActive:  false,
 	commentsOverlay:        false,
 	commentsEmbeddedActive: false,
@@ -15,57 +15,57 @@ var annotationsPattern = {
 	commentsGathered:       { "commentOverlay": "on", "comments": { } },
 	trackedElements:        [ ],
 	targetOrigin:           (window.location.protocol == "file:") ? "*" : window.location.protocol+"//"+window.location.host,
-	
+
 	/**
 	* record which annotations are related to this pattern so they can be sent to the viewer when called
 	*/
 	gatherComments: function() {
-		
+
 		// make sure this only added when we're on a pattern specific view
 		if (document.getElementById("sg-patterns") === null) {
-			
+
 			var count = 0;
-			
+
 			for (comment in comments.comments) {
 				var item = comments.comments[comment];
 				var els  = document.querySelectorAll(item.el);
 				if (els.length > 0) {
-					
+
 					count++;
 					item.displaynumber = count;
-					
+
 					for (var i = 0; i < els.length; ++i) {
 						els[i].onclick = (function(item) {
 							return function(e) {
-								
+
 								if (annotationsPattern.commentsOverlayActive) {
-									
+
 									e.preventDefault();
 									e.stopPropagation();
-									
+
 									// if an element was clicked on while the overlay was already on swap it
 									var obj = JSON.stringify({ "displaynumber": item.displaynumber, "el": item.el, "title": item.title, "comment": item.comment });
 									parent.postMessage(obj,annotationsPattern.targetOrigin);
-									
+
 								}
-								
+
 							}
 						})(item);
 					}
 				}
-				
-				
+
+
 			}
-			
+
 		} else {
-			
+
 			var obj = JSON.stringify({ "commentOverlay": "off" });
 			parent.postMessage(obj,annotationsPattern.targetOrigin);
-			
+
 		}
-		
+
 	},
-	
+
 	/**
 	* embed a comment by building the sg-annotations div (if necessary) and building an sg-annotation div
 	* @param  {Object}      element to check the parent node of
@@ -73,22 +73,22 @@ var annotationsPattern = {
 	* @param  {String}      the comment HTML
 	*/
 	embedComments: function (el,title,comment) {
-		
+
 		// build the annotation div and add the content to it
 		var annotationDiv = document.createElement("div");
 		annotationDiv.classList.add("sg-annotation");
-		
+
 		var h3       = document.createElement("h3");
 		var p        = document.createElement("p");
 		h3.innerHTML = title;
 		p.innerHTML  = comment;
-		
+
 		annotationDiv.appendChild(h3);
 		annotationDiv.appendChild(p);
-		
+
 		// find the parent element to attach things to
 		var parentEl = annotationsPattern.findParent(el);
-		
+
 		// see if a child with the class annotations exists
 		var els = parentEl.getElementsByClassName("sg-annotations");
 		if (els.length > 0) {
@@ -99,17 +99,17 @@ var annotationsPattern = {
 			annotationsDiv.appendChild(annotationDiv);
 			parentEl.appendChild(annotationsDiv);
 		}
-		
+
 	},
-	
+
 	/**
 	* recursively find the parent of an element to see if it contains the sg-pattern class
 	* @param  {Object}      element to check the parent node of
 	*/
 	findParent: function(el) {
-		
+
 		var parentEl;
-		
+
 		if (el.classList.contains("sg-pattern")) {
 			return el;
 		} else if (el.parentNode.classList.contains("sg-pattern")) {
@@ -117,27 +117,27 @@ var annotationsPattern = {
 		} else {
 			parentEl = annotationsPattern.findParent(el.parentNode);
 		}
-		
+
 		return parentEl;
-		
+
 	},
-	
+
 	/**
 	* toggle the annotation feature on/off
 	* based on the great MDN docs at https://developer.mozilla.org/en-US/docs/Web/API/window.postMessage
 	* @param  {Object}      event info
 	*/
 	receiveIframeMessage: function(event) {
-		
+
 		var data = (typeof event.data !== "string") ? event.data : JSON.parse(event.data);
-		
+
 		// does the origin sending the message match the current host? if not dev/null the request
 		if ((window.location.protocol != "file:") && (event.origin !== window.location.protocol+"//"+window.location.host)) {
 			return;
 		}
-		
+
 		if ((data.resize !== undefined) && (annotationsPattern.commentsOverlayActive)) {
-			
+
 			for (var i = 0; i < annotationsPattern.trackedElements.length; ++i) {
 				var el = annotationsPattern.trackedElements[i];
 				if (window.getComputedStyle(el.element,null).getPropertyValue("max-height") == "0px") {
@@ -150,22 +150,22 @@ var annotationsPattern = {
 					parent.postMessage(obj,annotationsPattern.targetOrigin);
 				}
 			}
-			
+
 		} else if (data.commentToggle !== undefined) {
-			
+
 			var i, els, item, displayNum;
-			
+
 			// if this is an overlay make sure it's active for the onclick event
 			annotationsPattern.commentsOverlayActive  = false;
 			annotationsPattern.commentsEmbeddedActive = false;
-			
+
 			// see which flag to toggle based on if this is a styleguide or view-all page
 			if ((data.commentToggle === "on") && (document.getElementById("sg-patterns") !== null)) {
 				annotationsPattern.commentsEmbeddedActive = true;
 			} else if (data.commentToggle === "on") {
 				annotationsPattern.commentsOverlayActive  = true;
 			}
-			
+
 			// if comments overlay is turned off make sure to remove the has-annotation class and pointer
 			if (!annotationsPattern.commentsOverlayActive) {
 				els = document.querySelectorAll(".has-annotation");
@@ -177,7 +177,7 @@ var annotationsPattern = {
 					els[i].style.display = "none";
 				}
 			}
-			
+
 			// if comments embedding is turned off make sure to hide the annotations div
 			if (!annotationsPattern.commentsEmbeddedActive) {
 				els = document.getElementsByClassName("sg-annotations");
@@ -185,57 +185,57 @@ var annotationsPattern = {
 					els[i].style.display = "none";
 				}
 			}
-			
+
 			// if comments overlay is turned on add the has-annotation class and pointer
 			if (annotationsPattern.commentsOverlayActive) {
-				
+
 				var count = 0;
-				
+
 				for (i = 0; i < comments.comments.length; i++) {
 					item = comments.comments[i];
 					els  = document.querySelectorAll(item.el);
-					
+
 					var state = true;
-					
+
 					if (els.length) {
-						
+
 						count++;
-						
+
 						//Loop through all items with annotations
 						for (k = 0; k < els.length; k++) {
-							
+
 							els[k].classList.add("has-annotation");
-							
+
 							var span       = document.createElement("span");
 							span.innerHTML = count;
 							span.classList.add("annotation-tip");
-							
+
 							if (window.getComputedStyle(els[k],null).getPropertyValue("max-height") == "0px") {
 								span.style.display = "none";
 								state = false;
 							}
-							
+
 							annotationsPattern.trackedElements.push({ "itemel": item.el, "element": els[k], "displayNumber": count, "state": state });
-							
+
 							els[k].insertBefore(span,els[k].firstChild);
-							
+
 						}
-						
+
 					}
-					
+
 				}
-				
+
 				// count elements so it can be used when displaying the results in the viewer
 				var count = 0;
-				
+
 				// iterate over the comments in annotations.js
 				for(i = 0; i < comments.comments.length; i++) {
-					
+
 					var state = true;
-					
+
 					var item  = comments.comments[i];
 					var els   = document.querySelectorAll(item.el);
-					
+
 					// if an element is found in the given pattern add it to the overall object so it can be passed when the overlay is turned on
 					if (els.length > 0) {
 						count++;
@@ -246,15 +246,15 @@ var annotationsPattern = {
 						}
 						annotationsPattern.commentsGathered.comments[count] = { "el": item.el, "title": item.title, "comment": item.comment, "number": count, "state": state };
 					}
-				
+
 				}
-				
+
 				// send the list of annotations for the page back to the parent
 				var obj = JSON.stringify(annotationsPattern.commentsGathered);
 				parent.postMessage(obj,annotationsPattern.targetOrigin);
-				
+
 			} else if (annotationsPattern.commentsEmbeddedActive && !annotationsPattern.commentsEmbedded) {
-				
+
 				// if comment embedding is turned on and comments haven't been embedded yet do it
 				for (i = 0; i < comments.comments.length; i++)  {
 					item = comments.comments[i];
@@ -264,21 +264,21 @@ var annotationsPattern = {
 					}
 					annotationsPattern.commentsEmbedded = true;
 				}
-				
+
 			} else if (annotationsPattern.commentsEmbeddedActive && annotationsPattern.commentsEmbedded) {
-				
+
 				// if comment embedding is turned on and comments have been embedded simply display them
 				els = document.getElementsByClassName("sg-annotations");
 				for (i = 0; i < els.length; ++i) {
 					els[i].style.display = "block";
 				}
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 };
 
 // add the onclick handlers to the elements that have an annotations
--- core/styleguide/js/code-viewer.js
+++ core/styleguide/js/code-viewer.js
@@ -7,7 +7,7 @@
  */
 
 var codeViewer = {
-	
+
 	// set up some defaults
 	codeActive:   false,
 	tabActive:    "e",
@@ -17,12 +17,12 @@ var codeViewer = {
 	ids:          { "e": "#sg-code-title-html", "m": "#sg-code-title-mustache", "c": "#sg-code-title-css" },
 	targetOrigin: (window.location.protocol === "file:") ? "*" : window.location.protocol+"//"+window.location.host,
 	copyOnInit:   false,
-	
+
 	/**
 	* add the onclick handler to the code link in the main nav
 	*/
 	onReady: function() {
-		
+
 		// not sure this is needed anymore...
 		$('body').addClass('code-ready');
 
@@ -31,71 +31,71 @@ var codeViewer = {
 				codeViewer.slideCode($('#sg-code-container').outerHeight());
 			}
 		});
-		
+
 		// add the onclick handler
 		$('#sg-t-code').click(function(e) {
-			
+
 			e.preventDefault();
-			
+
 			// remove the class from the "eye" nav item
 			$('#sg-t-toggle').removeClass('active');
-			
+
 			// if the code link in the main nav is active close the panel. otherwise open
 			if ($(this).hasClass('active')) {
 				codeViewer.closeCode();
 			} else {
 				codeViewer.openCode();
 			}
-			
+
 		});
-		
+
 		// initialize the code viewer
 		codeViewer.codeContainerInit();
-		
+
 		// load the query strings in case code view has to show by default
 		var queryStringVars = urlHandler.getRequestVars();
 		if ((queryStringVars.view !== undefined) && ((queryStringVars.view === "code") || (queryStringVars.view === "c"))) {
 			codeViewer.copyOnInit = ((queryStringVars.copy !== undefined) && (queryStringVars.copy === "true")) ? true : false;
 			codeViewer.openCode();
 		}
-		
+
 	},
-	
+
 	/**
 	* decide on if the code panel should be open or closed
 	*/
 	toggleCode: function() {
-		
+
 		if (!codeViewer.codeActive) {
 			codeViewer.openCode();
 		} else {
 			codeViewer.closeCode();
 		}
-		
+
 	},
-	
+
 	/**
 	* after clicking the code view link open the panel
 	*/
 	openCode: function() {
-		
+
 		// make sure the annotations overlay is off before showing code view
 		$('#sg-t-annotations').removeClass('active');
 		annotationsViewer.commentsActive = false;
 		var obj = JSON.stringify({ "commentToggle": "off" });
 		document.getElementById('sg-viewport').contentWindow.postMessage(obj,codeViewer.targetOrigin);
 		annotationsViewer.slideComment(999);
-		
+
 		// tell the iframe code view has been turned on
 		var obj = JSON.stringify({ "codeToggle": "on" });
 		document.getElementById('sg-viewport').contentWindow.postMessage(obj,codeViewer.targetOrigin);
-		
+
 		// note it's turned on in the viewer
 		codeViewer.codeActive = true;
 		$('#sg-t-code').addClass('active');
-		
+
 	},
-	
+
 	/**
 	* after clicking the code view link close the panel
 	*/
@@ -106,46 +106,46 @@ var codeViewer = {
 		codeViewer.slideCode($('#sg-code-container').outerHeight());
 		$('#sg-t-code').removeClass('active');
 	},
-	
+
 	/**
 	* add the basic mark-up and events for the code container
 	*/
 	codeContainerInit: function() {
-		
+
 		// the bulk of this template is in core/templates/index.mustache
 		if (document.getElementById("sg-code-container") === null) {
 			$('<div id="sg-code-container" class="sg-view-container"></div>').html($("#code-template").html()).appendTo('body').css('bottom',-$(document).outerHeight());
 			setTimeout(function(){ $('#sg-code-container').addClass('anim-ready'); },50); //Add animation class once container is positioned out of frame
 		}
-		
+
 		// make sure the close button handles the click
 		$('body').delegate('#sg-code-close-btn','click',function() {
 			codeViewer.closeCode();
 			return false;
 		});
-		
+
 		// make sure the click events are handled on the HTML tab
 		$(codeViewer.ids["e"]).click(function() {
 			codeViewer.swapCode("e");
 		});
-		
+
 		// make sure the click events are handled on the Mustache tab
 		$(codeViewer.ids["m"]).click(function() {
 			codeViewer.swapCode("m");
 		});
-		
+
 		// make sure the click events are handled on the CSS tab
 		$(codeViewer.ids["c"]).click(function() {
 			codeViewer.swapCode("c");
 		});
-		
+
 	},
-	
+
 	/**
 	* depending on what tab is clicked this swaps out the code container. makes sure prism highlight is added.
 	*/
 	swapCode: function(type) {
-		
+
 		codeViewer.clearSelection();
 		var fill      = "";
 		var className = (type == "c") ? "css" : "markup";
@@ -163,7 +163,7 @@ var codeViewer = {
 		$('.sg-code-title-active').removeClass('sg-code-title-active');
 		$(codeViewer.ids[type]).toggleClass("sg-code-title-active");
 	},
-	
+
 	/**
 	* select the code where using cmd+a/ctrl+a
 	*/
@@ -176,7 +176,7 @@ var codeViewer = {
 			selection.addRange(range);
 		}
 	},
-	
+
 	/**
 	* clear any selection of code when swapping tabs or opening a new pattern
 	*/
@@ -189,14 +189,14 @@ var codeViewer = {
 			}
 		}
 	},
-	
+
 	/**
 	* slides the panel
 	*/
 	slideCode: function(pos) {
 		$('#sg-code-container').css('bottom',-pos);
 	},
-	
+
 	/**
 	* once the AJAX request for the encoded mark-up is finished this runs.
 	* if the encoded tab is the current active tab it adds the content to the default code container
@@ -207,7 +207,7 @@ var codeViewer = {
 			codeViewer.activateDefaultTab("e",this.responseText);
 		}
 	},
-	
+
 	/**
 	* once the AJAX request for the mustache mark-up is finished this runs.
 	* if the mustache tab is the current active tab it adds the content to the default code container
@@ -218,7 +218,7 @@ var codeViewer = {
 			codeViewer.activateDefaultTab("m",this.responseText);
 		}
 	},
-	
+
 	/**
 	* once the AJAX request for the css mark-up is finished this runs. if this function is running then css has been enabled
 	* if the css tab is the current active tab it adds the content to the default code container
@@ -230,7 +230,7 @@ var codeViewer = {
 			codeViewer.activateDefaultTab("c",this.responseText);
 		}
 	},
-	
+
 	/**
 	* when loading the code view make sure the active tab is highlighted and filled in appropriately
 	*/
@@ -254,16 +254,16 @@ var codeViewer = {
 			codeViewer.copyOnInit = false;
 		}
 	},
-	
+
 	/**
 	* when turning on or switching between patterns with code view on make sure we get
 	* the code from from the pattern via post message
 	*/
 	updateCode: function(lineage,lineageR,patternPartial,patternState,cssEnabled) {
-		
+
 		// clear any selections that might have been made
 		codeViewer.clearSelection();
-		
+
 		// draw lineage
 		if (lineage.length !== 0) {
 			var lineageList = "";
@@ -277,7 +277,7 @@ var codeViewer = {
 		} else {
 			$("#sg-code-lineage").css("display","none");
 		}
-		
+
 		// draw reverse lineage
 		if (lineageR.length !== 0) {
 			var lineageRList = "";
@@ -291,7 +291,7 @@ var codeViewer = {
 		} else {
 			$("#sg-code-lineager").css("display","none");
 		}
-		
+
 		// when clicking on a lineage item change the iframe source
 		$('#sg-code-lineage-fill a, #sg-code-lineager-fill a').on("click", function(e){
 			e.preventDefault();
@@ -299,7 +299,7 @@ var codeViewer = {
 			var obj = JSON.stringify({ "path": urlHandler.getFileName($(this).attr("data-patternpartial")) });
 			document.getElementById("sg-viewport").contentWindow.postMessage(obj,codeViewer.targetOrigin);
 		});
-		
+
 		// show pattern state
 		if (patternState != "") {
 			$("#sg-code-patternstate").css("display","block");
@@ -308,25 +308,25 @@ var codeViewer = {
 		} else {
 			$("#sg-code-patternstate").css("display","none");
 		}
-		
+
 		// fill in the name of the pattern
 		$('#sg-code-lineage-patternname, #sg-code-lineager-patternname, #sg-code-patternstate-patternname').html(patternPartial);
-		
+
 		// get the file name of the pattern so we can get the various editions of the code that can show in code view
 		var fileName = urlHandler.getFileName(patternPartial);
-		
+
 		// request the encoded markup version of the pattern
 		var e = new XMLHttpRequest();
 		e.onload = this.saveEncoded;
 		e.open("GET", fileName.replace(/\.html/,".escaped.html") + "?" + (new Date()).getTime(), true);
 		e.send();
-		
+
 		// request the mustache markup version of the pattern
 		var m = new XMLHttpRequest();
 		m.onload = this.saveMustache;
 		m.open("GET", fileName.replace(/\.html/,".mustache") + "?" + (new Date()).getTime(), true);
 		m.send();
-		
+
 		// if css is enabled request the css for the pattern
 		if (cssEnabled) {
 			var c = new XMLHttpRequest();
@@ -334,28 +334,28 @@ var codeViewer = {
 			c.open("GET", fileName.replace(/\.html/,".css") + "?" + (new Date()).getTime(), true);
 			c.send();
 		}
-		
+
 		// move the code into view
 		codeViewer.slideCode(0);
-		
+
 		$("#sg-code-loader").css("display","none");
-		
+
 	},
-	
+
 	/**
 	* toggle the comment pop-up based on a user clicking on the pattern
 	* based on the great MDN docs at https://developer.mozilla.org/en-US/docs/Web/API/window.postMessage
 	* @param  {Object}      event info
 	*/
 	receiveIframeMessage: function(event) {
-		
+
 		var data = (typeof event.data !== "string") ? event.data : JSON.parse(event.data);
-		
+
 		// does the origin sending the message match the current host? if not dev/null the request
 		if ((window.location.protocol !== "file:") && (event.origin !== window.location.protocol+"//"+window.location.host)) {
 			return;
 		}
-		
+
 		// switch based on stuff related to the postmessage
 		if (data.codeOverlay !== undefined) {
 			if (data.codeOverlay === "on") {
@@ -387,9 +387,9 @@ var codeViewer = {
 				}
 			}
 		}
-		
+
 	}
-	
+
 };
 
 // when the document is ready make the codeViewer ready
