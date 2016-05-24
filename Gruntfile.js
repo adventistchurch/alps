3485f75bc63b1189f58f027bb4c05eb163a26c94
Brent Hardinge <hardingeb@gc.adventist.org>
2016-05-23 23:55:58 -0400

    trying to fix the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function(grunt) {
             patternlab: {
                 options: {
                     execOptions: {
+                        failOnError: false,
                         maxBuffer: 1024 * 1024 * 64
                         // maxBuffer: Infinity
                     },
