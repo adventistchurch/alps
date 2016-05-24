6c81d3d4120b6d66a9e3f094d690ec0c6b5f4ff7
Brent Hardinge <hardingeb@gc.adventist.org>
2016-05-23 23:30:48 -0400

    commenting out the images to test the dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,9 +164,9 @@ module.exports = function(grunt) {
     grunt.registerTask('dev', [
         'css',
         'javascript',
-        'shell:patternlab',
+        'shell:patternlab'
         // 'images',
-        'copy'
+        // 'copy'
     ]);
 
     /**
