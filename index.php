8f1478b6cf17abcabe81513755e9dbbb675652fc
Brent Hardinge <hardingeb@gc.adventist.org>
2016-08-04 21:45:20 -0400

    Adding links to the CDN and a better intro

--- index.php
+++ index.php
@@ -1,6 +1,125 @@
-<?php
-/* This will give an error. Note the output
- * above, which is before the header() call */
-header('Location: public/');
-exit;
-?>
+
+<!DOCTYPE html>
+<html class="pl theme--ming no-js" lang="en" >
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    <title>Welcome to ALPS, the Adventist Living Pattern System</title>
+    <meta name="description" content="Seventh-day Adventist beliefs are meant to permeate your whole life. Growing out of scriptures that paint a compelling portrait of God, you are invited to explore, experience and know the One who desires to make us whole.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat" media="all">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merriweather" media="all">
+    <link rel="stylesheet" type="text/css" href='//fonts.googleapis.com/css?family=Oswald' media="all">
+    <link rel="stylesheet" type="text/css" href="http://cdn.adventist.io/alps/2/latest/css/main.css" media="all">
+
+    <script>document.createElement("picture");</script>
+    <script src="http://cdn.adventist.io/alps/2/latest/js/head-script.min.js" type="text/javascript"></script>
+
+
+    <!-- Begin Pattern Lab (Required for Pattern Lab to run properly) -->
+    <link rel="stylesheet" href="/public/styleguide/css/styleguide.css" media="all">
+    <link rel="stylesheet" href="/public/styleguide/css/styleguide-specific.css" media="all" />
+
+    <!-- Custom styles for Pattern Lab -->
+    <link rel="stylesheet" href="/public/css/styleguide-custom.css" media="all" />
+
+    <!-- End Pattern Lab -->
+    <link rel="icon" href="/public/favicon.ico" />
+  </head>
+  <body class="body theme--warm">
+
+  <div class="layout-container spacing pre-header active">
+    <div class="alps-logo-wrap">
+      <a href="/" class="alps-logo"><img src="/public/images/logo-alps.svg" alt="ALPS - Adventist Living Pattern System"></a>
+    </div>
+    <div class="rel text">
+      <div class="sg-introduction">
+
+      <div class="cf">
+        <div class="shift-left--fluid pad-double">
+                  <h1 class="pad-half--btm font--tertiary--l">Welcome to the Adventist Living Pattern System</h1>
+        <p>We are glad you found ALPS. Take a look around, and see what you think. There is lots to explore. You can find <a href="https://github.com/adventistchurch/alps/">the code on Github</a>, check it out, <a href="https://github.com/adventistchurch/alps/issues">file issues</a>, and create pull requests. This code belongs to the Adventist church. It's ours!</p>
+
+          <p>The Adventist Living Pattern System (ALPS) is a set of design patterns created by the Seventh-day Adventist Church for use around the world by Adventist churches, schools, and organizational units. The purpose of ALPS is to make it easy for our church to create good looking sites that promote the brand of the church, while allowing for individuality and customization of each site.</p>
+          <p> As you browse through the pages of this site, you will see examples of patterns for different types of pages. From <a href="/public/?p=pages-home">home pages</a> to <a href="/public/?p=pages-channel-main-with-image">channel pages</a>, to <a href="/public/?p=pages-basic-page-with-image">basic pages</a>, ALPS is designed to be beautiful, fast and flexible, while maintaining a consistent brand across the sites that use it.</p>
+          <p>Take a look for yourself!</p>
+          <div class="pad-half--btm"><a href="/public/" class="btn theme--secondary-background-color link--white">View the Adventist Living Pattern System</a></div>
+
+          <h2 class="pad-half--btm font--tertiary--l">Include in Your Project</h2>
+          <p>Want to include ALPS in your project? The Adventist church is making the CSS and Javascript needed to build an ALPS site available via a public CDN. All you need to do to implement the latest styles is to link the files provides below and you will be up to date.</p>
+          <p>Here are the links you need to make it happen.</p>
+          <h3 class="pad-half--btm font--secondary--m">Header</h3>
+          <p>Include this code in the <code>&lt;head&gt;</code> of your page. This include the fonts, ALPS css, and the Javascript needed to start the page.</p>
+          <code class="sg-codesample pad" id="head_code" onclick="this.focus();this.select()">&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;//fonts.googleapis.com/css?family=Montserrat&quot; media=&quot;all&quot;&gt;<br/>&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;//fonts.googleapis.com/css?family=Merriweather&quot; media=&quot;all&quot;&gt;<br/>&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href='//fonts.googleapis.com/css?family=Oswald' media=&quot;all&quot;&gt;<br/>&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;http://cdn.adventist.io/alps/2/latest/css/main.css&quot; media=&quot;all&quot;&gt;<br/><br/>&lt;script&gt;document.createElement(&quot;picture&quot;);&lt;/script&gt;<br/>&lt;script src=&quot;http://cdn.adventist.io/alps/2/latest/js/head-script.min.js&quot; type=&quot;text/javascript&quot;&gt;&lt;/script&gt;</code>
+
+          <h3 class="pad-half--btm font--secondary--m">Footer</h3>
+          <p>Include this code in the footer of your page, right before the closing <code>&lt;/body&gt;</code> tag.</p>
+          <code class="sg-codesample pad">&lt;script src=&quot;https://code.jquery.com/jquery-2.2.4.min.js&quot; &gt;&lt;/script&gt;<br/>&lt;script src=&quot;http://cdn.adventist.io/alps/2/latest/js/script.min.js&quot; type=&quot;text/javascript&quot;&gt;&lt;/script&gt;
+          </code>
+
+        </div>
+        <div class="shift-right--fluid pad-double">
+          <h3 class="font--tertiary--m pad-half--btm">Follow on GitHub</h3>
+          <p class="font--primary--xs">ALPS is available on GitHub as a public project where you can download, file issues and pull requests, and follow the progress. </p>
+          <p><a class="github-button" href="https://github.com/adventistchurch/alps" data-icon="octicon-eye" data-style="mega" data-count-href="/adventistchurch/alps/watchers" data-count-api="/repos/adventistchurch/alps#subscribers_count" data-count-aria-label="# watchers on GitHub" aria-label="Watch adventistchurch/alps on GitHub">Watch</a>
+          <a class="github-button" href="https://github.com/adventistchurch/alps" data-icon="octicon-star" data-style="mega" data-count-href="/adventistchurch/alps/stargazers" data-count-api="/repos/adventistchurch/alps#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star adventistchurch/alps on GitHub">Star</a>
+          <a class="github-button" href="https://github.com/adventistchurch/alps/fork" data-icon="octicon-repo-forked" data-style="mega" data-count-href="/adventistchurch/alps/network" data-count-api="/repos/adventistchurch/alps#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork adventistchurch/alps on GitHub">Fork</a>
+          <a class="github-button" href="https://github.com/adventistchurch/alps/issues" data-icon="octicon-issue-opened" data-style="mega" data-count-api="/repos/adventistchurch/alps#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue adventistchurch/alps on GitHub">Issue</a></p>
+        </div>
+      </div> <!-- /.g-2up--70-30--at-medium -->
+
+
+
+      </div>
+
+
+    </div>
+  </div>
+<footer class="footer" role="contentinfo">
+  <div class="footer__inner cf bg--medium-brown white can-be--dark-dark">
+    <div class="footer__legal bg--brown  can-be--dark-light">
+      <div class="footer__legal__inner layout-container spacing--quarter--until-large">
+        <div class="footer__unify-copyright-address spacing--quarter--until-large">
+          <p class="footer__copyright font--secondary--xs brown--light no-space--btm">Copyright &copy; 2016, General Conference of Seventh-day Adventists</p>
+          <address class="footer__address font--secondary--xs brown--light">12501 Old Columbia Pike, Silver Spring, MD 20904, USA 301-680-6000</address>
+        </div> <!-- /.footer__unify-copyright-address -->
+        <div class="footer__legal-links font--secondary--xs"><a class="hover brown--light space-half--right" href="https://www.adventist.org/en/copyright/trademark-and-logo-usage/">Trademark and Logo Usage</a> <a class="hover brown--light" href="https://www.adventist.org/en/copyright/legal-notice/">Legal Notice</a></div>
+      </div> <!-- /.layout-container -->
+    </div> <!-- /.legal -->
+  </div> <!-- /.footer__inner -->
+</footer> <!-- /.footer -->
+
+
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <script src="https://cdn.jsdelivr.net/clipboard.js/1.5.12/clipboard.min.js"></script>
+    <script>
+/* from clipboard.js example */
+
+// var clipboard = new Clipboard('.copyIt');
+
+// clipboard.on('success', function(e) {
+//     console.info('Action:', e.action);
+//     console.info('Text:', e.text);
+//     console.info('Trigger:', e.trigger);
+
+//     console.log('success! you got it.');
+
+//     e.clearSelection();
+// });
+
+// clipboard.on('error', function(e) {
+//     console.error('Action:', e.action);
+//     console.error('Trigger:', e.trigger);
+// });
+
+// var clipboard = new Clipboard('.btn');
+// clipboard.destroy();
+
+    </script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" ></script>
+    <script src="http://cdn.adventist.io/alps/2/latest/js/script.min.js" type="text/javascript"></script>
+
+  </body>
+</html>
