5239ddf1e6fa98c66d8daeb3fe9be46104e024f9
Brent Hardinge <hardingeb@gc.adventist.org>
2016-08-24 15:20:09 -0400

    ADDED: A new example pattern that shows both grids and layouts and all their current options. This replaces their individual layout and grid pattern pages.

--- source/_data/annotations.js
+++ source/_data/annotations.js
@@ -10,6 +10,16 @@ var comments = {
 		"el": ".secondary-colors",
 		"title" : "Secondary Colors",
 		"comment" : "<p>The two secondary colors, 'Warm' and 'Cool' allow some further customization to secondary links and color bars throughout the templates.</p>"
+	},
+	{
+		"el": ".grids",
+		"title": "Grids",
+		"comment" : "<p>ALPS has a grid system based on Flexbox that provides some basic grid functionality. These should always be used inside the <a href='/public/?p=molecules-layouts'>Layout patterns.</a>"
+	},
+	{
+		"el" : ".layouts",
+		"title" : "Layouts",
+		"comment" : "<p>Layout containers keep content centered and within a maximum width. Also adjusts left and right padding as the viewport widens. To adjust content inside the layout containers, use the <a href='/public/?p=molecules-grids'>Grid patterns.</a></p>"
 	}
 ]
 };
