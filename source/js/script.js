4e81d3124903450351d8f439398af17fa5d1655f
Kelsey Cahill <kcahill@southleft.com>
2018-01-19 08:41:39 -0500

    Header development

--- source/js/script.js
+++ source/js/script.js
@@ -44,6 +44,53 @@
     getWidth();
   }
 
+  // var $nav = $('.c-priority-nav');
+  // var $btn = $('.c-priority-nav c-priority-nav__toggle');
+  // var $vlinks = $('.c-priority-nav .c-priority-nav--visible');
+  // var $hlinks = $('.c-priority-nav .c-priority-nav--hidden');
+  // var breaks = [];
+  //
+  // function updateNav() {
+  //   var availableSpace = $btn.hasClass('is-hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+  //   // The visible list is overflowing the nav
+  //   if($vlinks.width() > availableSpace) {
+  //     // Record the width of the list
+  //     breaks.push($vlinks.width());
+  //     // Move item to the hidden list
+  //     $vlinks.children().last().prependTo($hlinks);
+  //     // Show the dropdown btn
+  //     if($btn.hasClass('is-hidden')) {
+  //       $btn.removeClass('is-hidden');
+  //     }
+  //   // The visible list is not overflowing
+  //   } else {
+  //     // There is space for another item in the nav
+  //     if(availableSpace > breaks[breaks.length-1]) {
+  //       // Move the item to the visible list
+  //       $hlinks.children().first().appendTo($vlinks);
+  //       breaks.pop();
+  //     }
+  //     // Hide the dropdown btn if hidden list is empty
+  //     if(breaks.length < 1) {
+  //       $btn.addClass('is-hidden');
+  //       $hlinks.addClass('is-hidden');
+  //     }
+  //   }
+  //   // Recur if the visible list is still overflowing the nav
+  //   if($vlinks.width() > availableSpace) {
+  //     updateNav();
+  //   }
+  // }
+  //
+  // // Window listeners
+  // $(window).resize(function() {
+  //     updateNav();
+  // });
+  // $btn.on('click', function() {
+  //   $hlinks.toggleClass('is-hidden');
+  // });
+  // updateNav();
+
   // SITE SPECIFIC JS ------------------------------//
 
   var toggleClasses = function(element) {
