90411bc4dc301f8b324754df693780c65fba9f2e
Brent Hardinge <designerbrent@gmail.com>
2019-09-27 19:07:20 -0400

    Adding the version increment and CHANGELOG.md entries.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
    * of /cdn/<major_version/<version>/ that contains the javascript and css.
    */
   var major_version = "3";
-  var version = "3.0.1";
+  var version = "3.0.2";
 
   grunt.initConfig({
     pkg: pkg,
