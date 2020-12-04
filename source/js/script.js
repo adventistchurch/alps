20c48322c3e596a9f21604ef93bfb23ccd3d0feb
David Eleuterius <davideleuterius@gmail.com>
2020-12-04 16:00:57 -0600

    search progress
    - added typehead style mockup
    - now handles proper theme color application

--- source/js/script.js
+++ source/js/script.js
@@ -485,6 +485,23 @@
     $(this).toggleClass('active');
   });
 
+  // MOCKING TYPEAHEAD FUNCTIONALITY
+  var searchInput = $('#search-input');
+  searchInput.keyup(function(){
+    var cnt = searchInput.val().length;
+    if ( cnt > 0) {
+      $('[data-context="search-main-suggestions"]').slideDown(200);
+    } else {
+      $('[data-context="search-main-suggestions"]').slideUp(200);
+    }
+  })
+  if (searchInput.blur && searchInput.val().length == 0) {
+      $('[data-context="search-main-suggestions"]').slideUp(200);
+  }
+
+  // ------- END MOCK TYPEAHEAD
+
+
 })(window.alpsJQueryRef); // Fully reference jQuery after this point.
 
 // Overwrites native 'firstElementChild' prototype.
