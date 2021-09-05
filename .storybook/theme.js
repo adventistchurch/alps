0ee65ae2f047bbeae72b5415ad721cc93d52b5f0
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2021-09-05 18:48:40 +0300

    #541 PART 3. Update storybook settings

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -5,7 +5,7 @@ const themeColor = 'light';
 export default create({
     base: themeColor, // Is this a 'light' or 'dark' theme?
 
-    brandTitle: 'ALPS-Storybook',
+    brandTitle: 'ALPS Library',
     brandUrl: 'https://github.com/adventistchurch/alps/tree/storybook',
     brandImage: themeColor === 'light' ? 'alps-storybook-logo.svg' : 'alps-storybook-logo-dark.svg',
 
