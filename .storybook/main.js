8dd01ad576a9a71ccf8b14e08e073ba257d613d5
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2021-09-05 14:30:22 +0300

    #541 PART 3. Update main js file. Return addon-links

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
         "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
     "addons": [
+        "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/preset-create-react-app",
         '@whitespace/storybook-addon-html',
