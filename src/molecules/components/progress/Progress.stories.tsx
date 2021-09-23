34e598b10c0c09233f05e938d0431ddc1ff29dd1
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2021-09-23 23:08:31 +0300

    #543 PART 4. Add molecules Progress component

---
+++ src/molecules/components/progress/Progress.stories.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import {Meta, Story} from "@storybook/react";
+import {Progress, ProgressProps, sizeMap} from "./Progress";
+import {Button} from "../../../components/button/Button";
+
+export default {
+    title: "molecules/components/Progress",
+    parameters: {
+        componentSubtitle: 'Component',
+        status: 'released'
+    },
+    component: Progress,
+    argTypes: {
+        fixed: {
+            defaultValue: true,
+            control: {type: "boolean"}
+        },
+        percentage: {
+            options: { min: 0, max: 100 },
+            control: {type: "number"}
+        },
+        size: {
+            defaultValue: sizeMap.medium,
+            control: {type: "select"}
+        },
+        visible: {
+            control: {type: "boolean"}
+        }
+    }
+} as Meta;
+
+const Template: Story<ProgressProps> = (args) => <Progress {...args} />
+
+export const Basic = Template.bind({});
+
+export const With_percentage = Template.bind({});
+With_percentage.args = {
+    percentage: 50
+}
+
+export const Hairline = Template.bind({});
+Hairline.args = {
+    size: "hairline"
+}
+
+export const Small = Template.bind({});
+Small.args = {
+    size: "small"
+}
+
+export const Medium = Template.bind({});
+Medium.args = {
+    size: "medium"
+}
+
+export const Large = Template.bind({});
+Large.args = {
+    size: "large"
+}
+
+export const Huge = Template.bind({});
+Huge.args = {
+    size: "huge"
+}
+
+const FixedTemplate: Story<ProgressProps> = (args) => {
+    const [percentage, setPercentage] = useState(50);
+    return (
+        <div className={"u-padding--double u-spacing--double"}>
+            <Progress {...args} percentage={percentage} />
+            <h1>
+                Scroll untill you see <em>Hello!</em>
+            </h1>
+
+            {[1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map(x => (
+                <div key={`p-${x}`}>
+                    <Button label={`Set to ${x}%`} onClick={() => setPercentage(x)} />
+                </div>
+            ))}
+            <h1 style={{ marginTop: '50vh' }}>
+                <em>Hello!</em>
+            </h1>
+        </div>
+    )
+}
+
+export const Fixed = FixedTemplate.bind({});
+Fixed.args = {
+    fixed: true
+}
---
+++ src/molecules/components/progress/Progress.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+
+export const sizes = {
+    hairline: '1pt',
+    small: '3pt',
+    medium: '5pt',
+    large: '10pt',
+    huge: '16pt',
+};
+
+const widthTransition = 'width .5s ease-out';
+const heightTransition = 'height .3s ease-out';
+
+export const sizeMap = {
+    'hairline':'hairline',
+    'small':'small',
+    'medium':'medium',
+    'large':'large',
+    'huge':'huge'
+}
+
+export interface ProgressProps {
+    fixed?: boolean,
+    percentage?: number,
+    size?: keyof typeof sizeMap,
+    style?: [],
+    visible?: boolean
+}
+
+export const Progress = ({fixed, percentage, size = "medium", style, visible = true}: ProgressProps): JSX.Element => {
+    const barStyle = useBarStyle(visible, size, fixed, style)
+    const progressStyle = useProgressStyle(percentage, visible, size)
+
+    return (
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        <div className="u-theme--background-color--lighter" style={barStyle}>
+            {/*/
+            / eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore*/}
+            <div className="u-theme--background-color--dark" style={progressStyle} />
+        </div>
+    )
+}
+
+const useBarStyle = (
+    visible: boolean,
+    size: keyof typeof sizeMap,
+    fixed = false,
+    style = {},
+    transition = heightTransition
+) => {
+    const fixedProps = fixed
+        ? { position: 'fixed', top: 0, left: 0, right: 0, zIndex: 999999 }
+        : {}
+
+    return {
+        ...fixedProps,
+        height: visible ? sizes[size] : 0,
+        transition: transition,
+        ...style,
+    }
+}
+
+const useProgressStyle = (percentage: number | undefined, visible: boolean, size: keyof typeof sizeMap) => {
+    const [fakePercentage, setFakePercentage] = useState(0)
+    const barStyle = useBarStyle(
+        visible,
+        size,
+        false,
+        {},
+        `${widthTransition}, ${heightTransition}`
+    )
+
+    const isPercentageSet =
+        percentage !== null && percentage !== undefined;
+
+    useEffect(() => {
+        let interval: any;
+        if (isPercentageSet) {
+            setFakePercentage(0)
+            clearInterval(interval)
+        } else {
+            interval = setInterval(() => {
+                if (fakePercentage < 100) {
+                    setFakePercentage(fakePercentage + 20)
+                } else {
+                    clearInterval(interval)
+                }
+            }, 500)
+        }
+        return () => clearInterval(interval)
+    }, [fakePercentage, isPercentageSet])
+
+    return {
+        width: `${
+            isPercentageSet ? (percentage > 100 ? 100 : percentage) : fakePercentage
+        }%`,
+        ...barStyle,
+    }
+}
