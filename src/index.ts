1d063d7d80857ed72538ec34f0f2e57476f5867a
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2021-11-08 12:21:35 +0300

    #544 PART 5. Migration from Pattern Lab to Storybook Prepare build to NPM includes: - Full components' folder - Molecules folder: blocks, churchLocator, components

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,50 @@
-//Components
+// ----- Components -----
 export {Button} from "./components/button/Button";
+export {Grid} from "./components/grids/Grid";
+export {Icon} from "./components/icons/Icon";
+export {IconWrap} from "./components/icons/IconWrap";
 
+//images
+export {Image} from "./components/images/Image";
+export {Picture} from "./components/images/Picture";
+
+//lists
+export {Definition} from "./components/lists/definition/Definition";
+export {Inline} from "./components/lists/inline/Inline";
+export {Ordered} from "./components/lists/ordered/Ordered";
+export {Unordered} from "./components/lists/unordered/Unordered";
+
+//table
+
+//text
 export {Blockquote} from "./components/text/Blockquote";
 export {Caption} from "./components/text/Caption";
 export {Dropcap} from "./components/text/Dropcap";
 export {Intro} from "./components/text/Intro";
 export {Kicker} from "./components/text/Kicker";
 
-export {Definition} from "./components/lists/definition/Definition";
-
-export {Icon} from "./components/icons/Icon";
-export {IconWrap} from "./components/icons/IconWrap";
-export {Image} from "./components/images/Image";
-export {Picture} from "./components/images/Picture";
 export {Video} from "./components/video/Video";
 
-//Molecules - blocks
+// ----- Molecules - blocks        -----
 export {BreakoutBlock} from "./molecules/blocks/breackoutBlock/BreakoutBlock";
+export {ContentBlock} from "./molecules/blocks/contentBlock/ContentBlock";
 export {ContentBlockExpand} from "./molecules/blocks/contentBlockExpand/ContentBlockExpand";
+export {CtaBlock} from "./molecules/blocks/ctaBlock/CtaBlock";
 export {GalleryBlock} from "./molecules/blocks/galleryBlock/GalleryBlock";
 export {HeadingBlock} from "./molecules/blocks/headingBlock/HeadingBlock";
-export {CtaBlock} from "./molecules/blocks/ctaBlock/CtaBlock";
+export {MediaBlock} from "./molecules/blocks/mediaBlock/MediaBlock";
 
-//Molecules - components
+// ----- Molecules - churchLocator -----
+export {ChurchInfo} from "./molecules/churchLocator/churchInfo/ChurchInfo";
+export {ChurchInfoLarge} from "./molecules/churchLocator/churchInfoLarge/ChurchInfoLarge";
+export {SearchResultsItem} from "./molecules/churchLocator/searchResultsItem/SearchResultsItem";
+
+// ----- Molecules - components    -----
 export {Accordion} from "./molecules/components/accordion/Accordion";
 export {Comment} from "./molecules/components/comment/Comment";
 export {Comments} from "./molecules/components/comments/Comments";
 export {Progress} from "./molecules/components/progress/Progress";
 export {ShareTools} from "./molecules/components/share/ShareTools";
 export {Slide} from "./molecules/components/slide/Slide";
+
+// ----- Molecules - forms         -----
