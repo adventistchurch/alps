8896f04428f43dd7dce2c043b633ded5742f7921
gabyortova <gabi.ortova@gmail.com>
2025-06-16 19:16:53 +0300

    - Form - fields are not used anymore, only children - ErrorMessage - add red color to error messages - BaseInput - add onChange, onBlur and required property - FormLabel - add  '*' when field is required - TextField - add validations

--- src/molecules/forms/elements/BaseInput.tsx
+++ src/molecules/forms/elements/BaseInput.tsx
@@ -1,40 +1,72 @@
-import React, {MouseEventHandler} from "react";
+import React, {MouseEventHandler} from 'react';
 
-import useClasses from "../../../helpers/useClasses";
-import useInputFocus from "../../../helpers/useInputFocus";
+import useClasses from '../../../helpers/useClasses';
+import useInputFocus from '../../../helpers/useInputFocus';
 
 export interface BaseInputProps {
-    checked?: boolean,
-    error?: string,
-    hasFocus?: boolean,
-    id?: string,
-    name?: string,
-    placeholder?: string,
-    type?: "checkbox" | "email" | "password" | "number" | "radio" | "search" | "text" | "textarea",
-    value?: string,
-    onClick?: (event: MouseEventHandler<HTMLAnchorElement>) => void
+  checked?: boolean;
+  error?: string;
+  hasFocus?: boolean;
+  id?: string;
+  name?: string;
+  placeholder?: string;
+  type?:
+    | 'checkbox'
+    | 'email'
+    | 'password'
+    | 'number'
+    | 'radio'
+    | 'search'
+    | 'text'
+    | 'textarea';
+  value?: string;
+  required?: boolean;
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onClick?: (
+    e: React.MouseEvent<HTMLInputElement | HTMLTextAreaElement, MouseEvent>
+  ) => void;
+  onBlur?: (
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
 }
 
-export const BaseInput = ({checked, error, type = "text", value, hasFocus = false, name, placeholder, onClick}: BaseInputProps): JSX.Element => {
-    const inputClass = useClasses("form-input", {
-        "has-error": !!error,
-    })
-    const inputFocusRef = useInputFocus(hasFocus);
+export const BaseInput = ({
+  checked,
+  error,
+  type = 'text',
+  value,
+  hasFocus = false,
+  name,
+  placeholder,
+  required = false,
+  onChange,
+  onClick,
+  onBlur
+}: BaseInputProps): JSX.Element => {
+  const inputClass = useClasses('form-input', {
+    'has-error': !!error
+  });
+  const inputFocusRef = useInputFocus(hasFocus);
 
-    const isTextArea = type === "textarea";
+  const isTextArea = type === 'textarea';
 
-    return React.createElement(
-        isTextArea ? "textarea" : "input",
-        {
-            className: inputClass,
-            defaultChecked: checked,
-            defaultValue: value,
-            ref: inputFocusRef,
-            type,
-            name: name,
-            placeholder: placeholder,
-            onClick: isTextArea ? null : onClick
-        },
-        isTextArea ? value : null
-    )
-}
+  return React.createElement(
+    isTextArea ? 'textarea' : 'input',
+    {
+      className: inputClass,
+      defaultChecked: checked,
+      defaultValue: value,
+      ref: inputFocusRef,
+      type,
+      name: name,
+      placeholder: placeholder,
+      required: required,
+      onChange: onChange,
+      onClick: onClick,
+      onBlur: onBlur
+    },
+    isTextArea ? value : null
+  );
+};
--- src/molecules/forms/elements/Form.tsx
+++ src/molecules/forms/elements/Form.tsx
@@ -1,107 +1,128 @@
-import React, {useCallback} from "react";
+import React, {useCallback} from 'react';
 
-import useClasses from "../../../helpers/useClasses";
-import {Dropdown} from "./Dropdown";
-import {Button} from "../../../atoms/button/Button";
-import {darkThemeClass, themeBackgroundClass} from "../../../global/colors";
-import {OptionGroup} from "./OptionGroup";
-import {TextField} from "./TextField";
-import Title from "../../../atoms/text/Title";
-import {getBaseClass} from "../../../global/commons";
+import useClasses from '../../../helpers/useClasses';
+// import {Dropdown} from "./Dropdown";
+// import {Button} from "../../../atoms/button/Button";
+import {darkThemeClass, themeBackgroundClass} from '../../../global/colors';
+// import {OptionGroup} from "./OptionGroup";
+// import {TextField} from "./TextField";
+import Title from '../../../atoms/text/Title';
+import {getBaseClass} from '../../../global/commons';
 
-const components = {
-    title: {component: Title},
-    text: {component: TextField, getType: (type: string) => type},
-    "checkbox-group": {component: OptionGroup, getType: () => "checkbox"},
-    "radio-group": {component: OptionGroup, getType: () => "radio"},
-    select: {component: Dropdown},
-    button: {component: Button},
-    submit: {component: Button},
-}
+//no sence
+// const components = {
+//     title: {component: Title},
+//     text: {component: TextField, getType: (type: string) => type},
+//     "checkbox-group": {component: OptionGroup, getType: () => "checkbox"},
+//     "radio-group": {component: OptionGroup, getType: () => "radio"},
+//     select: {component: Dropdown},
+//     button: {component: Button},
+//     submit: {component: Button},
+// }
+//no sence
+// function useFormFields(fields: FormField[], children: React.ReactNode) {
+//   if (children) return children;
 
-function useFormFields(fields: [], children: React.ReactNode) {
-    if (children) return children
+//    return fields.map(({type, name, id}, key) => {
+//       const {component, getType} = components[type] || components["text"]
 
-    return fields.map(({type, name, id}, key) => {
-        const {component, getType} = components[type] || components["text"]
+//       return React.createElement(component, {
+//           name,
+//           id: id || name,
+//           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//           // @ts-ignore
+//           type: getType ? getType(type) : undefined,
+//           key,
+//       })
+//   })
+// }
 
-        return React.createElement(component, {
-            name,
-            id: id || name,
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            type: getType ? getType(type) : undefined,
-            key,
-        })
-    })
-}
+export type FormFieldType =
+  | 'title'
+  | 'text'
+  | 'checkbox-group'
+  | 'radio-group'
+  | 'select'
+  | 'button'
+  | 'submit';
+
+export type FormField = {
+  type: FormFieldType;
+  name: string;
+  id?: string;
+};
 
 export interface FormProps {
-    action?: string,
-    children?: React.ReactNode,
-    className?: string,
-    darkMode?: boolean,
-    labelPosition?: "top" | "bottom",
-    fields?: [],
-    inline?: boolean,
-    method?: "post" | "get",
-    noValidate?: boolean,
-    onSubmit?: (e: any) => void,
-    role?: "search",
-    title?: React.ReactNode
+  action?: string;
+  children?: React.ReactNode;
+  className?: string;
+  darkMode?: boolean;
+  labelPosition?: 'top' | 'bottom';
+  fields?: FormField[];
+  inline?: boolean;
+  method?: 'post' | 'get';
+  noValidate?: boolean;
+  onSubmit?: (e: any) => void;
+  role?: 'search';
+  title?: React.ReactNode;
 }
 
 export const Form = ({
-                         children,
-                         className,
-                         darkMode = false,
-                         fields = [],
-                         inline = false,
-                         labelPosition,
-                         onSubmit,
-                         title
-                     }: FormProps): JSX.Element => {
-    const formClass = useClasses(
-        "c-form",
-        {
-            "c-form--inline": inline,
-            [darkThemeClass]: darkMode,
-        },
-        className
-    )
+  children,
+  className,
+  darkMode = false,
+  fields = [],
+  inline = false,
+  labelPosition,
+  onSubmit,
+  title
+}: FormProps): JSX.Element => {
+  const formClass = useClasses(
+    'c-form',
+    {
+      'c-form--inline': inline,
+      [darkThemeClass]: darkMode
+    },
+    className
+  );
 
-    const formFields = useFormFields(fields, children);
+  const formFields = children; //useFormFields(fields, children);
 
-    const _onSubmit = useCallback(
-        (e: { preventDefault: () => void; }) => {
-            if (typeof onSubmit === "function") {
-                e.preventDefault()
-                onSubmit(e)
-            }
-        },
-        [onSubmit]
-    )
+  const _onSubmit = useCallback(
+    (e: {preventDefault: () => void}) => {
+      if (typeof onSubmit === 'function') {
+        e.preventDefault();
+        onSubmit(e);
+      }
+    },
+    [onSubmit]
+  );
 
-    return (
-        <form className={formClass + ` ${darkMode ? getBaseClass(themeBackgroundClass, ["darker"]) : null}`}
-              onSubmit={_onSubmit}>
-            {title ? (
-                typeof title === "string" ? (
-                    <Title
-                        className={"u-text--strong"}
-                        color={darkMode ? "white" : null}
-                        fontSize="l"
-                        text={title}
-                        //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                        // @ts-ignore
-                        themeColor={darkMode ? null : "darker"}
-                    />
-                ) : (
-                    title
-                )
-            ) : null}
+  return (
+    <form
+      className={
+        formClass +
+        ` ${darkMode ? getBaseClass(themeBackgroundClass, ['darker']) : null}`
+      }
+      onSubmit={_onSubmit}
+    >
+      {title ? (
+        typeof title === 'string' ? (
+          <Title
+            className={'u-text--strong'}
+            color={darkMode ? 'white' : null}
+            fontSize="l"
+            text={title}
+            //eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            themeColor={darkMode ? null : 'darker'}
+          />
+        ) : (
+          title
+        )
+      ) : null}
 
-            {formFields}
-        </form>
-    )
-}
+      {formFields}
+    </form>
+  );
+};
