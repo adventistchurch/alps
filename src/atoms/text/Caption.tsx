09ac7f213b87b1b87e6d28ce25df1662472448d5
gabyortova <gabi.ortova@gmail.com>
2025-06-23 18:57:36 +0300

    Dropdown - add value Caption - add children, content is optional

--- src/atoms/text/Caption.tsx
+++ src/atoms/text/Caption.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 
 export interface CaptionProps {
-    /**
-     * Caption content
-     */
-    content: string
+  /**
+   * Caption content
+   */
+  content?: string;
+  children: ReactNode;
 }
 
-export const Caption = ({content = "Caption content"}: CaptionProps): JSX.Element => {
-    return (
-        <div className={"o-caption u-color--gray u-font--secondary--s"}>
-            {content}
-        </div>
-    );
-}
+export const Caption = ({
+  content,
+  children
+}: CaptionProps): JSX.Element => {
+  return (
+    <div className={'text o-caption u-color--gray u-font--secondary--s'}>
+      {content || children}
+    </div>
+  );
+};
