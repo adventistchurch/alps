b70850b89ff390f991222f134ff7d7138da5c6f6
gabyortova <gabi.ortova@gmail.com>
2025-07-04 16:31:58 +0300

    Fixed PR requests:
    Revert changes in presets and ArticleContent

--- src/organisms/content/articleContent/ArticleContent.tsx
+++ src/organisms/content/articleContent/ArticleContent.tsx
@@ -20,7 +20,7 @@ export interface ArticleContentProps {
 export const ArticleContent = ({
                                    children,
                                    sidebar,
-                                   hasDropcap = false,
+                                   hasDropcap = true,
                                    spacing = "double"
                                }: ArticleContentProps): JSX.Element => {
     return (
